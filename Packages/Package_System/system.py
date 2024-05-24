b8b3b9164481a3586b14ff46ca9944e3baaf99f7
FedericoDeniard <fededeniard@gmail.com>
2024-05-24 09:08:59 -0300

    removed folders

--- Packages/Package_System/system.py
+++
@@ -1,9 +0,0 @@
-import platform
-from os import system
-
-def clear_screen():
-    os_name = platform.system()
-    if os_name == "Windows":
-        clear_screen()
-    else:
-        system("clear")
