7187bed5588be4962d71261f10d4de5ffeecfe7a
FedericoDeniard <fededeniard@gmail.com>
2024-05-24 08:58:22 -0300

    .

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from Packages.Package_Menus.consult import *
 from Packages.Package_Menus.modify import *
 from Packages.Package_System.system import *
 
-
 def main():
         while True:
                 option = get_int(message="1. Cargar datos\n2. Consultar datos\n3. Salir\n",min=1,max=3,error_message="Numeros validos del 1 al 3")
